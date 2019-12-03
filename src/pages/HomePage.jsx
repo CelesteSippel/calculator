85d331d1ce0ff07c9396feb13e226719adf2ba5e
Celeste Sippel <celestechristine@gmail.com>
2019-12-02 20:49:54 -0500

    unable to / 3 digit numbers

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,100 @@
-import React from 'react'
-import HelloWorld from '../components/HelloWorld'
+import React, { useState } from 'react'
 
 const HomePage = () => {
-  return <HelloWorld />
+  const [input, setInput] = useState([])
+
+  const clickButton = value => {
+    setInput(prev => {
+      return prev + value
+    })
+  }
+
+  const calculate = () => {
+    console.log('calculating')
+    if (input[1] === '-') {
+      setInput(parseInt(input[0]) - parseInt(input[2]))
+    } else if (input[1] === '+') {
+      setInput(parseInt(input[0]) + parseInt(input[2]))
+    } else if (input[1] === '*') {
+      setInput(parseInt(input[0]) * parseInt(input[2]))
+    } else if (input[1] === '/') {
+      setInput(parseInt(input[0]) / parseInt(input[2]))
+    }
+  }
+
+  const clearScreen = () => {
+    setInput([])
+  }
+
+  return (
+    <>
+      <div className="app">
+        <div className="wrapper">
+          <div className="input">{input}</div>
+
+          <div className="row">
+            <button className="clear-button" onClick={() => clearScreen()}>
+              AC
+            </button>
+          </div>
+          <div className="row">
+            <button className="numbers" onClick={() => clickButton(7)}>
+              7
+            </button>
+            <button className="numbers" onClick={() => clickButton(8)}>
+              8
+            </button>
+            <button className="numbers" onClick={() => clickButton(9)}>
+              9
+            </button>
+            <button className="operator" onClick={() => clickButton('/')}>
+              /
+            </button>
+          </div>
+
+          <div className="row">
+            <button className="numbers" onClick={() => clickButton(4)}>
+              4
+            </button>
+            <button className="numbers" onClick={() => clickButton(5)}>
+              5
+            </button>
+            <button className="numbers" onClick={() => clickButton(6)}>
+              6
+            </button>
+            <button className="operator" onClick={() => clickButton('*')}>
+              x
+            </button>
+          </div>
+          <div className="row">
+            <button className="numbers" onClick={() => clickButton(1)}>
+              1
+            </button>
+            <button className="numbers" onClick={() => clickButton(2)}>
+              2
+            </button>
+            <button className="numbers" onClick={() => clickButton(3)}>
+              3
+            </button>
+            <button className="operator" onClick={() => clickButton('-')}>
+              -
+            </button>
+          </div>
+          <div className="row">
+            <button className="numbers" onClick={() => clickButton(0)}>
+              0
+            </button>
+            <button className="operator" onClick={() => calculate()}>
+              =
+            </button>
+            <button className="operator" onClick={() => clickButton('+')}>
+              +
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default HomePage
